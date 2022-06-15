f43e20bb99407f9fa683befeb09f2037e265d363
Armando <hello.armandocrn@gmail.com>
2022-06-15 16:19:23 +0200

    First check for empty input done

--- script.js
+++ script.js
@@ -29,3 +29,79 @@ la classe .correct-input oppure .wrong-input
 
 MI SA CHE L'ENTER NON DEVO METTERLO IO, C'è GIà IN AUTOMATICO.
 */
+
+// COMPONENTS
+const form = document.querySelector("form"); // Non so se serve
+const submitBtn = document.querySelector("#submit");
+
+const name = document.querySelector("#first-name");
+const errorName = document.querySelector(".name-error");
+
+const email = document.querySelector("#email");
+const errorEmail = document.querySelector(".email-error");
+
+const pass = document.querySelector("#password");
+const errorPass = document.querySelector(".pass-error");
+
+const passConf = document.querySelector("#conf-password");
+const errorConfPass = document.querySelector(".conf-pass-error");
+
+// EVENTS
+form.addEventListener("submit", (e) => console.log("non so")); //non credo serva
+submitBtn.addEventListener("click", btnChecker);
+
+// name.addEventListener("input", );
+
+// FUNCTIONs
+
+/*
+    Creare un add event listener per ogni singolo input type input
+    setTime dopo qualche secondo 0.3/0.5s
+    e SE ha la classe empty-imput RIMUOVILA 
+    e ovviamente rimuovi il contenuto dell'error, almeno per quanto riguardo
+    l'essere un input vuoto
+    Cerca di automatizzare anche questo, visto che c'è un botto di codice da ripetere
+  */
+
+function btnChecker(e) {
+  e.preventDefault();
+
+  checkEmpty(name);
+  checkEmpty(email);
+  checkEmpty(pass);
+  checkEmpty(passConf);
+
+  /*
+  Altre cose da fare??
+  Se è tutto completato invia il form e basta(?);
+  Tanto per la validazione dei singoli input la si farà mentre si scrive
+  */
+}
+
+function checkEmpty(input) {
+  let content = input.value;
+  let error;
+
+  switch (input) {
+    case name:
+      error = errorName;
+      break;
+
+    case email:
+      error = errorEmail;
+      break;
+
+    case pass:
+      error = errorPass;
+      break;
+
+    case passConf:
+      error = errorConfPass;
+      break;
+  }
+
+  if (content === "") {
+    error.innerText = "Please complete this field";
+    input.classList.add("empty-imput");
+  }
+}
