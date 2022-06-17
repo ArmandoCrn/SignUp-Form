33785252cf0fe3cdf15a04c643f37d131afa3d7c
Armando <hello.armandocrn@gmail.com>
2022-06-17 18:04:36 +0200

    Before change span

--- script.js
+++ script.js
@@ -1,36 +1,13 @@
 /*
-
-EMAIL: "Please enter a valid email address"; 
-
-CONFIRM PASS: "Password mismatch"
-
 PASSWORD:
 Per la pass il discorso è diverso. Dovremo creare 3 p dove all'interno ci 
 saranno rispettivamente
-Minimum 8 characters
+✖Minimum 8 characters
 At least one number
 At least one character
 
-Man mano che le cose vengono rispettate aggiungeremo la class .correct,
-facendo diventare verse il testo
-
-OLTRE a far cambiare il testo, dovremo aggiungere agl'input sopracitati, tranne name
-la classe .correct-input oppure .wrong-input
-
 MI SA CHE L'ENTER NON DEVO METTERLO IO, C'è GIà IN AUTOMATICO.
-*/
-/*
-per automatizzare la validazione di EMAIL PASS e CONFIRM PASS
 
-il bordo diventa rosso quando è sbagliato intutti e 3
-
-in EMAIL e conf pass compare sotto solo una stringa
-
-in EMAIL la stringa sarà
-"Please enter a valid email address"; 
-
-in CONFIRM PASS la stringa sarà
-"Password mismatch"
 */
 
 // COMPONENTS
@@ -38,16 +15,17 @@ const form = document.querySelector("form"); // Non so se serve
 const submitBtn = document.querySelector("#submit");
 
 const name = document.querySelector("#first-name");
-const errorName = document.querySelector(".name-error");
+const errorName = document.querySelector("#name-error");
 
 const email = document.querySelector("#email");
-const errorEmail = document.querySelector(".email-error");
+const errorEmail = document.querySelector("#email-error");
 
 const pass = document.querySelector("#password");
-const errorPass = document.querySelector(".pass-error");
+const errorPass = document.querySelector("#pass-error");
+const passDisplay = document.querySelector(".pass-condition");
 
 const passConf = document.querySelector("#conf-password");
-const errorPassConf = document.querySelector(".conf-pass-error");
+const errorPassConf = document.querySelector("#conf-pass-error");
 
 // EVENTS
 form.addEventListener("submit", () => console.log("Done")); //non credo serva
@@ -58,23 +36,24 @@ name.addEventListener("keyup", delay(validateName, 300));
 email.addEventListener("keyup", delay(validateEmail, 300));
 email.addEventListener("focus", () => {
   if (email.value !== "") {
-    validateEmail;
+    validateEmail();
   }
 });
 email.addEventListener("blur", () => removeOkSign(errorEmail));
 
 pass.addEventListener("keyup", delay(validatePass, 300));
+pass.addEventListener("focus", passFocusEvent);
+pass.addEventListener("blur", passBlurEvent);
 
 passConf.addEventListener("keyup", delay(validateConfPass, 300));
 passConf.addEventListener("focus", () => {
   if (email.value !== "") {
-    validateConfPass;
+    validateConfPass();
   }
 });
 passConf.addEventListener("blur", () => removeOkSign(errorPassConf));
 
 // FUNCTIONS
-
 //Funzione DELAY presa da stackoverflow:
 //(https://stackoverflow.com/questions/1909441/how-to-delay-the-keyup-handler-until-the-user-stops-typing)
 function delay(callback, ms) {
@@ -114,19 +93,23 @@ function showPass() {
 
 function validatePass() {
   inputFull(pass, errorPass);
-  //FIXME: cerca online validation pass
-  /*
-  OCCHIO CHE quando scrivi, deve anche controllare il confirm pass e farlo diventare wrong,
-  
-  // Cambia dinamicamente anche il Conf Pass
-  if(confirmpass.value !== "") {
-    if (passConf.value !== pass.value) {
-    wrongValidation(passConf, errorPassConf);
+  const regexpPass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/.test(pass.value);
+
+  //Change color and symbol ✓/🗴
+
+  if (!regexpPass) {
+    wrongValidation(pass, errorPass);
   } else {
-    correctValidation(passConf, errorPassConf);
+    correctValidation(pass, errorPass);
   }
-}
-    */
+
+  if (passConf.value !== "") {
+    if (passConf.value !== pass.value) {
+      wrongValidation(passConf, errorPassConf);
+    } else {
+      correctValidation(passConf, errorPassConf);
+    }
+  }
 }
 
 function validateConfPass() {
@@ -176,7 +159,9 @@ function changeErrorMessage(input) {
       break;
 
     case pass:
-      "sdf";
+      if (pass.value !== "") {
+        message = "Please enter a valid password";
+      }
       break;
 
     case passConf:
@@ -214,5 +199,33 @@ function removeOkSign(error) {
   error.classList.remove("correct");
   if (error.innerText === "✓") {
     error.innerText = "";
+  }
+}
+
+//PASS FUNCTION
+function passFocusEvent() {
+  if (pass.value !== "") {
+    validatePass();
+  }
+  errorPass.classList.remove("min-height");
+  passDisplay.removeAttribute("id");
+}
+
+function passBlurEvent() {
+  if (pass.value !== "") {
+    validatePass();
+  }
+  passDisplay.id = "d-none";
+  errorPass.classList.add("min-height");
+  removeOkSign(errorPass);
+}
+
+function checkPass(span, status) {
+  const lengthSpan = document.querySelector(".span-length");
+  const lowerSpan = document.querySelector(".span-lowercase");
+  const upperSpan = document.querySelector(".span-uppercase");
+  const numberSpan = document.querySelector(".span-number");
+
+  if (pass.length >= 8) {
   }
 }
